5595db3224b8489ed31598a93b7c8c85912fdfc0
Pavan-Mp <pavanyadavak777coorg@gmail.com>
2023-12-19 13:48:24 +0530

    second commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full-stack Web developer"],
+        strings: [" Full-stack Web developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
